c9df1ac031b2dcef497674e34dde1073b7d35f45
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-07-19 11:59:44 +0100

    Modify start urls ,test scrap title

--- hukd/hukd/spiders/hotuk.py
+++ hukd/hukd/spiders/hotuk.py
@@ -1,10 +1,18 @@
 import scrapy
-
+from scrapy.spiders import CrawlSpider,Rule
+from scrapy.linkextractors import LinkExtractor
 
 class HotukSpider(scrapy.Spider):
     name = "hotuk"
     allowed_domains = ["hotukdeals.com"]
-    start_urls = ["https://hotukdeals.com"]
+    start_urls = ["https://hotukdeals.com/new"]
+    
+    #rules = [Rule(LinkExtractor(allow=))]
 
     def parse(self, response):
-        pass
+        yield{
+            'title':response.xpath('//*[@id="thread_4169668"]/div/div[3]/strong/a/text()').get(),
+            'title-2nd':response.xpath('//article//strong/a/text()').get()
+        }
+#//*[@id="thread_4169668"]/div/div[3]/strong
+#//*[@id="thread_4169668"]/div/div[3]/strong/a
